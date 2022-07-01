593ef69bab7b7b1de44af821cd0c12c682377c13
Marcus Hellberg <marcus@vaadin.com>
2022-07-01 14:39:53 -0700

    nicer formatting

--- frontend/views/empty/empty-view.ts
+++ frontend/views/empty/empty-view.ts
@@ -21,10 +21,16 @@ export class EmptyView extends View {
 
   render() {
     return html`
-      <vaadin-message-list class="flex-grow" .items=${this.formattedMessages}></vaadin-message-list>
+      <vaadin-message-list
+        class="flex-grow"
+        .items=${this.formattedMessages}></vaadin-message-list>
       <div class="flex p-s gap-s items-baseline">
-        <vaadin-text-field placeholder="Name" @change=${this.userNameChange}></vaadin-text-field>
-        <vaadin-message-input class="flex-grow" @submit=${this.submit}></vaadin-message-input>
+        <vaadin-text-field
+          placeholder="Name"
+          @change=${this.userNameChange}></vaadin-text-field>
+        <vaadin-message-input
+          class="flex-grow"
+          @submit=${this.submit}></vaadin-message-input>
       </div>
     `;
   }
@@ -43,6 +49,8 @@ export class EmptyView extends View {
   connectedCallback() {
     super.connectedCallback();
     this.classList.add('flex', 'flex-col', 'h-full', 'box-border');
-    ChatEndpoint.join().onNext((message) => (this.messages = [...this.messages, message]));
+    ChatEndpoint.join().onNext(
+      (message) => (this.messages = [...this.messages, message])
+    );
   }
 }
